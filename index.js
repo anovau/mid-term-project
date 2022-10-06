bf5a7ca22eb38ca09599233645f2fbd9b07c88fb
anovau <novau_678@hotmail.com>
2022-10-06 19:27:56 +0200

    burger nav

---
+++ index.js
@@ -0,0 +1,6 @@
+const burgerIcon = document.querySelector("#burger");
+const navBarMenu = document.querySelector("#nav-links")
+
+burgerIcon.addEventListener("click", () => {
+    navBarMenu.classList.toggle("is-active")
+})
